ea2ae22d4b13ff88a3f1fc5434603c5e5fe19da1
keller0707 <kellersedillo@hotmail.com>
2021-12-09 20:26:03 -0700

    Clean up Client

--- udp_client.c
+++ udp_client.c
@@ -35,46 +35,40 @@ int main(int argc, char **argv[]){
 	//argv[3] = File Name
 	
 	//Set Port
-	int port = 9002;                                          //Default Port
-	if(argc == 4) port = strtol((char *)argv[2], NULL, 10);   //Set Port
-	printf("Port: %d\n", port);
+	int port = 9002;                                          // Default Port
+	if(argc == 4) port = strtol((char *)argv[2], NULL, 10);   // Set Port
+	printf("Port: %d\n", port);                               // Print Port
 
 	//Create Socket
-	int network_socket;
-	network_socket = socket(AF_INET, SOCK_DGRAM, 0);
-
-	//Check to see if socket is created.
-	if(network_socket == -1){
-		perror("Failed to create socket");
-		exit(EXIT_FAILURE);
-	}else printf("Socket Successfully Created!\n");
+	int network_socket;                                // Create boolean
+	network_socket = socket(AF_INET, SOCK_DGRAM, 0);   // Create Socket
+	if(network_socket == -1){                          // Check Socket
+		perror("Failed to create socket");         // Print Failure
+		exit(EXIT_FAILURE);                        // End Program
+	}else printf("Socket Successfully Created!\n");    // Print Success
 	//End if
 
 	//Specify an address for the socket
 	struct sockaddr_in server_address;
-	server_address.sin_family      = AF_INET;       //Type of Address
-	server_address.sin_port        = htons(port);   //Convert Port #
-	server_address.sin_addr.s_addr = INADDR_ANY;    //Send Address
+	server_address.sin_family      = AF_INET;       // Type of Address
+	server_address.sin_port        = htons(port);   // Convert Port #
+	server_address.sin_addr.s_addr = INADDR_ANY;    // Send Address
 
 	
-	//Send Message to Server
-	//char * msg = "Hello From Client!";     
-	printf("File Name: %s\n",argv[3]);                                                                                                  //Create MSG
-	int message_sent = sendto(network_socket, (const char *)argv[3], strlen((char *)argv[3]), 0, (struct sockaddr *)&server_address, sizeof(server_address));   //Send MSG
-	if(message_sent != -1) printf("Message sent to Server!");                                                                                         //Check if sent
-	else perror("Fail to send");
+	//Send File Name to Server
+	printf("File Name: %s\n",argv[3]);                                                                                                             // Print File Name
+	int message_sent;                                                                                                                              // Create boolean
+	messafe_sent = sendto(network_socket, (char*)argv[3], strlen((char*)argv[3]), 0, (struct sockaddr*)&server_address, sizeof(server_address));   // Send data
+	if(message_sent != -1) printf("Message sent to Server!");                                                                                      // Print Success
+	else perror("Fail to send");                                                                                                                   // Print Failure
  	
 	
 	//Receive Message from Server
-	char buffer[200];
-	socklen_t len = 0;
-        int receive = recvfrom(network_socket, (char *)buffer, 200, MSG_WAITALL, 0, &len); 
-	//int receive = recv(network_socket, &buffer, sizeof(buffer), 0); 
-	//int receive = recvfrom(network_socket, (char *)buffer, 200, 0, (struct sockaddr *)&server_address, &lens);   //Get data
-	//if(receive == -1) perror("Failed to receive message");
-	//else printf("Received Message\n");
-        buffer[receive] = '\0';                                                            //Add space to message
-	printf("Message:\n%s", buffer);                                                     //Print Msg 
+	char buffer[200];                                                                    // Create Buffer
+	socklen_t len = 0;                                                                   // Create length
+        int receive = recvfrom(network_socket, (char *)buffer, 200, MSG_WAITALL, 0, &len);   // Receive Data 
+	if(receive == -1) perror("Fail to receive package");	                             // Print Error
+	printf("Message:\n%s", buffer);                                                      // Print Message
 
 	
 	//Close the Socket
